fd652978b22cdee397548c0e6e91ee1b25a91191
Kaneki3332 <gabi.mihnea46@gmail.com>
2026-03-10 09:08:42 +0200

    lab2

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -1,17 +1,56 @@
 package org.example;
 
+import java.util.*;
+import java.util.stream.Stream;
+
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        IO.println(String.format("Hello and welcome!"));
 
-        for (int i = 1; i <= 5; i++) {
-            //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-            // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-            IO.println("i = " + i);
+        List<Integer> x = new ArrayList();
+        List<Integer> y = new ArrayList();
+        List<Integer> xPlusY = new ArrayList(); //a
+        Set<Integer> zSet = new TreeSet(); //b
+        List<Integer> xMinusY = new ArrayList();//c
+        int p = 4;
+        List<Integer> xPlusYLimitedByP = new ArrayList();
+        Random generator=new Random();
+      for(int i=0;i<5;i++)
+      {
+          x.add(generator.nextInt(11));
+      }
+Collections.sort(x);
+      for(int i=0;i<x.size();i++)
+          System.out.print(x.get(i)+" ");
+        System.out.println();
+
+        for(int i=0;i<7;i++)
+        {
+            y.add(generator.nextInt(11));
         }
+Collections.sort(y);
+        for(int i=0;i<y.size();i++)
+            System.out.print(y.get(i)+" ");
+       xPlusY.addAll(x);
+       xPlusY.addAll(y);
+        System.out.println();
+       for(int i=0;i<xPlusY.size();i++)
+           System.out.print(xPlusY.get(i)+" ");
+       zSet.addAll(x);
+       zSet.retainAll(y);
+        for(Integer i :zSet) {
+            System.out.print(i+" ");
+        }
+        xMinusY.addAll(x);
+        xMinusY.removeAll(y);
+        System.out.println();
+        for(int i=0;i<xMinusY.size();i++)
+            System.out.print(xMinusY.get(i)+" ");
+        for(Integer i:xPlusY)
+            if(i<=p) xPlusYLimitedByP.add(i);
+        System.out.println();
+        for(int i=0;i<xPlusYLimitedByP.size();i++)
+            System.out.print(xPlusYLimitedByP.get(i)+" ");
     }
 }
